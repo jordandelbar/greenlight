e1a1dcb75fc490cfc8c5ef025a7cf01ec059bcdd
Jordan Delbar <jordan.delbar@exotec.com>
2024-05-29 18:50:26 +0200

    Chapter 3.3 Encoding Structs

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"strconv"
 )
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+}
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
 	js, err := json.Marshal(data)
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/julienschmidt/httprouter"
+	"greenlight.jordandelbar.com/internal/data"
 	"net/http"
-	"strconv"
+	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,13 +12,24 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not proceed your request", http.StatusInternalServerError)
+	}
 }

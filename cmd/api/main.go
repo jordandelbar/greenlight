c945d939ca071beba781aa6ebb8d65becf123d5c
Jordan Delbar <jordandelbar@gmail.com>
2024-07-19 16:40:28 +0200

    Chapter 13.5 Graceful Shutdown of Background Tasks

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,7 @@ import (
 	"greenlight.jordandelbar.com/internal/mailer"
 	"log/slog"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -42,6 +43,7 @@ type application struct {
 	logger *slog.Logger
 	models data.Models
 	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {

2eeb700309f8164ecb301a6036999761e0a80222
Jordan Delbar <jordandelbar@gmail.com>
2024-08-21 15:43:33 +0200

    Chapter 14.3 Sending Activation Tokens

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"errors"
+	"net/http"
+	"time"
+
 	"greenlight.jordandelbar.com/internal/data"
 	"greenlight.jordandelbar.com/internal/validator"
-	"net/http"
 )
 
 func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -51,15 +53,19 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	go func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
-		if err != nil {
-			app.logger.Error(err.Error())
-		}
-	}()
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		info := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", info)
 		if err != nil {
 			app.logger.Error(err.Error())
 		}
--- go.mod
+++ go.mod
@@ -6,8 +6,8 @@ require (
 	github.com/go-mail/mail v2.3.1+incompatible
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/lib/pq v1.10.9
-	golang.org/x/crypto v0.25.0
-	golang.org/x/time v0.5.0
+	golang.org/x/crypto v0.26.0
+	golang.org/x/time v0.6.0
 )
 
 require (
